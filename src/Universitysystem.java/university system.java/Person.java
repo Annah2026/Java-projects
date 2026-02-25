f1820c22640251dab323498c88df390ae053aded
Annah2026 <anastacia.mwangi@strathmore.edu>
2026-02-25 10:29:56 +0300

    class person

--- src/Universitysystem.java/university system.java/Person.java
+++ src/Universitysystem.java/university system.java/Person.java
@@ -1,3 +1,16 @@
-public class Person {
+public abstract class Person {
+    Private String name;
+    Private int id;
+
+    Public person(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    Public String getname(){
+        return name;
+    }
+    Public int getid(){
+        return id;
+    }
     
 }
