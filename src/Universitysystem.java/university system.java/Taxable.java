6ea1fdf506a346a5cf4cbf7f4b1bb8a8385f4949
Annah2026 <anastacia.mwangi@strathmore.edu>
2026-02-26 01:18:48 +0300

    tax that payment must follow

---
+++ src/Universitysystem.java/university system.java/Taxable.java
@@ -0,0 +1,8 @@
+public class Taxable {
+    public interface Taxable {
+    // Standard tax rate for the school fees
+    double TAX_RATE = 0.15; 
+
+    double calculateTotal(double baseAmount);
+}
+
