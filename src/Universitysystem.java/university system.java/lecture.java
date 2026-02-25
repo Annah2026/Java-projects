09fcd2c95cad6a826cd04243e3850e4c6a6dd7b9
Annah2026 <anastacia.mwangi@strathmore.edu>
2026-02-25 20:24:51 +0300

    lecturer class

---
+++ src/Universitysystem.java/university system.java/lecture.java
@@ -0,0 +1,16 @@
+public Concrete class lecture {
+    Private String specialization;
+    Public String department;
+    Public lecturer ( String name,String id, String department){
+        Super(name,age,id);
+        this.specialization= specialization;
+    }
+    Public void showInfo(){
+        System.out.println("Lecturer:" + getName() +"Staffid:" + getid()+",Specialization:" +Specialization);
+    }
+}
+    
+    
+
+    
+}
